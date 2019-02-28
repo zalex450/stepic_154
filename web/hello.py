9de47998c217be880b9e5baea51c2a3f76074805
zalex450 <alexbk@mail.ru>
2019-02-28 11:14:09 +0300

    initial commit

---
+++ web/hello.py
@@ -0,0 +1,4 @@
+def task_1_9(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
