f567e0890befdc18aafa5bf02d6a56b7d225ad9c
Zalex450 <alexbk@mail.ru>
2019-03-23 17:39:37 +0000

    final version of task 2.11.1

--- web/ask/ask/settings.py
+++ web/ask/ask/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%5@1*i+&p^^#vmho4@s0*g+&l@(7n!w9rf#mokng01uv2cft&l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- web/ask/qa/urls.py
+++ web/ask/qa/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
-from qa.views import question
+from qa.views import test
 
-urlpatterns = patterns['qa.views',                                              
-   url(r'^$', 'test'),                                                              
-   url(r'^login/.*$', 'test', name='login'),                                    
-   url(r'^signup/.*', 'test', name='signup'),                                   
-   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),                 
-   url(r'^ask/.*', 'test', name='ask'),                                         
-   url(r'^popular/.*', 'test', name='popular'),                                 
-   url(r'^new/.*', 'test', name='new'),                                    
+urlpatterns = [
+   url(r'^$', test),                                                              
+   url(r'^login/.*$', test, name='login'),                                    
+   url(r'^signup/.*',test, name='signup'),                                   
+   url(r'^question/(?P<id>[0-9]+)/$', test, name='question'),                 
+   url(r'^ask/.*', test, name='ask'),                                         
+   url(r'^popular/.*', test, name='popular'),                                 
+   url(r'^new/.*', test, name='new'),                                    
 ]
